71e355bb6c0de5814f18cbe4ae0167d1526a72dc
Raj Bhatodra <91852356+raj-bhatodra@users.noreply.github.com>
2023-05-05 01:25:52 +0530

    Update settings.py

--- photoalbum/settings.py
+++ photoalbum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%%oek&+!@*g1k(qdwm$^)dgvjcxmoyig-pr(=3^7_dux_bbzcc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['65.2.34.134']
+ALLOWED_HOSTS = ['52.66.198.129']
 
 
 # Application definition
